170f633657e66d3a6cacc395bd3327e1fa492c33
aurorabreeze32 <aurorabreeze32@gmail.com>
2025-12-16 00:23:00 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import Poster, { Slice } from "@/components/Poster";
 
-const slices: Slice[] = [
-  { kind: "img", src: "/poster/Slice-1.png" },
-  { kind: "img", src: "/poster/Slice-2.png" },
-  { kind: "img", src: "/poster/Slice-3.png" },
-  { kind: "img", src: "/poster/Slice-4.png" },
-  { kind: "faq", src: "/poster/Slice-6-1.png" }, // FAQ лежит на Slice-6-1
-  { kind: "img", src: "/poster/Slice-5.png" },
-];
-
 export default function Page() {
-  return <Poster slices={slices} />;
+  return <Poster />;
 }
