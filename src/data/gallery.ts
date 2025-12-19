0f508556fda45af0c2f408a0a82b82307da53b78
aurorabreeze32 <aurorabreeze32@gmail.com>
2025-12-20 01:38:53 +0300

    Update gallery.ts

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,6 +1,23 @@
-// src/data/gallery.ts
-export const gallery = Array.from({ length: 19 }, (_, i) => {
-  return `/gallery/${String(i + 1).padStart(2, "0")}.png`;
-});
+export type GalleryItem = string | { full: string; alt?: string };
 
-export const slides = gallery.map((src) => ({ src }));
+export const gallery: GalleryItem[] = [
+  "/gallery/01.png",
+  "/gallery/02.png",
+  "/gallery/03.png",
+  "/gallery/04.png",
+  "/gallery/05.png",
+  "/gallery/06.png",
+  "/gallery/07.png",
+  "/gallery/08.png",
+  "/gallery/09.png",
+  "/gallery/10.png",
+  "/gallery/11.png",
+  "/gallery/12.png",
+  "/gallery/13.png",
+  "/gallery/14.png",
+  "/gallery/15.png",
+  "/gallery/16.png",
+  "/gallery/17.png",
+  "/gallery/18.png",
+  "/gallery/19.png",
+];
